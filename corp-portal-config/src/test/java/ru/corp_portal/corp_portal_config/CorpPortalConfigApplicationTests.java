cd82efea24a705cc0d419f700b21b3797d18f7f0
AndreiShev <shevelv_andrey@vk.com>
2025-01-26 00:00:28 +0300

    Init config service

---
+++ corp-portal-config/src/test/java/ru/corp_portal/corp_portal_config/CorpPortalConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.corp_portal.corp_portal_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpPortalConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
