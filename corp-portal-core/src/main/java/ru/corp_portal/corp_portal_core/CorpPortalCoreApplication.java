3a26d38b146db1e5b185a6c38e201bd42ab13455
AndreiShev <87591627+AndreiShev@users.noreply.github.com>
2025-01-25 16:38:37 +0300

    Initialize core service

---
+++ corp-portal-core/src/main/java/ru/corp_portal/corp_portal_core/CorpPortalCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.corp_portal.corp_portal_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpPortalCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpPortalCoreApplication.class, args);
+	}
+
+}
---
+++ corp-portal-core/src/test/java/ru/corp_portal/corp_portal_core/CorpPortalCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.corp_portal.corp_portal_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpPortalCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
