759bd9090d029c78eb078aa5217659ad5d62c3d7
AndreiShev <shevelv_andrey@vk.com>
2025-01-28 00:17:11 +0300

    update settings config server for vault

--- corp-portal-config/src/main/java/ru/corp_portal/corp_portal_config/CorpPortalConfigApplication.java
+++ corp-portal-config/src/main/java/ru/corp_portal/corp_portal_config/CorpPortalConfigApplication.java
@@ -3,9 +3,11 @@ package ru.corp_portal.corp_portal_config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableScheduling
 public class CorpPortalConfigApplication {
 
 	public static void main(String[] args) {
--- corp-portal-core/src/main/java/ru/corp_portal/corp_portal_core/CorpPortalCoreApplication.java
+++ corp-portal-core/src/main/java/ru/corp_portal/corp_portal_core/CorpPortalCoreApplication.java
@@ -2,8 +2,10 @@ package ru.corp_portal.corp_portal_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class CorpPortalCoreApplication {
 
 	public static void main(String[] args) {
